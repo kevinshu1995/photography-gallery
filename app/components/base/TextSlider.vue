ae2f1424c55796dfe779677716c5059bb0e85e90
wenshiu.hsu <wenshiu.hsu@gmail.com>
2025-10-31 17:18:26 +0800

    ✨ feat(home): add animated text slider to homepage
    - 新增 TextSlider.vue 組件，使用 anime.js 實現文字動畫效果
    - 在首頁用新的 TextSlider 組件取代靜態文字
    - 新增 animejs 作為專案依賴

---
+++ app/components/base/TextSlider.vue
@@ -0,0 +1,59 @@
+<template>
+    <ul class="relative overflow-hidden">
+        <li v-for="text in texts" :key="text" ref="animated-texts">{{ text }}</li>
+    </ul>
+</template>
+
+<script setup lang="ts">
+import { createTimeline, stagger, utils, splitText } from "animejs";
+
+const refTexts = useTemplateRef("animated-texts");
+
+const texts = computed(() => {
+    return [
+        "Moments, made lasting.",
+        "Light. Frame. Story.",
+        "From moments to meaning.",
+        "Every shutter tells a story.",
+        "Time pauses, life remains.",
+        "Time pauses, life remains.",
+        "Turning moments into memories.",
+    ];
+});
+
+onMounted(() => {
+    if (refTexts.value) {
+        const tl = createTimeline({
+            loop: true,
+            defaults: { ease: "inOut(3)", duration: 800 },
+        });
+        utils.$(refTexts.value).forEach(($text, index) => {
+            if (index !== 0) {
+                utils.set($text, { position: "absolute", left: "0", top: "0" });
+            }
+            const { words } = splitText($text as HTMLElement, {
+                words: { wrap: "clip" },
+                chars: true,
+            });
+            utils.set(words, { y: "100%", opacity: 0 });
+            tl.add(
+                words,
+                {
+                    y: ["100%", "0%"],
+                    opacity: 1,
+                },
+                stagger(100, { from: "random" })
+            ).add(
+                words,
+                {
+                    y: ["0%", "-100%"],
+                    opacity: 0,
+                    delay: 300,
+                },
+                stagger(100, { from: "random" })
+            );
+        });
+    }
+});
+</script>
+
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,12 +3,7 @@
         <header class="min-h-[70vh] flex items-center">
             <div>
                 <h1 class="uppercase text-[10vw] font-black leading-none">photography</h1>
-                <p>Moments, made lasting.</p>
-                <p>Light. Frame. Story.</p>
-                <p>From moments to meaning.</p>
-                <p>Every shutter tells a story.</p>
-                <p>Time pauses, life remains.</p>
-                <p>Turning moments into memories.</p>
+                <BaseTextSlider />
             </div>
         </header>
         <BaseImageGallery :gallery="gallery" v-if="gallery && gallery.length > 0" />
